a62856df1bb29845133ed07a24003694827edbf6
daleksprinter <b1015120@fun.ac.jp>
2019-05-01 22:10:04 +0900

    login authorization

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, request
+
 import pymysql.cursors
 
 
@@ -23,6 +24,7 @@ conn = pymysql.connect(host = host,
                         cursorclass = pymysql.cursors.DictCursor
 )
 
+'''
 @app.route('/')
 def index():
     with conn.cursor() as cursor:
@@ -31,9 +33,30 @@ def index():
         
         results = cursor.fetchall()
         return jsonify(results)
+'''
+
+@app.route("/api/login", methods = ['POST'])
+def login():
+
+    username = request.json['username']
+    password = request.json['password']
+     
+    with conn.cursor() as cursor:
+        sql = 'select * from user where username = %s'
+        cursor.execute(sql, username)
+
+        result = cursor.fetchall()
+        if len(result) == 0:
+            return make_response('login failed', 400)
+        if result[0]['password'] == password :
+            return jsonify(result)
+        else :
+            return make_response('login failed', 400)
+
+
 
 
 
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0')
+    app.run(host = '0.0.0.0', debug = True)
